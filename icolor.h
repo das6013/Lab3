34ce19b6ae30ae85526c71750afec733f6283158
Das3013 <46927879+das6013@users.noreply.github.com>
2022-06-30 12:59:36 +0700

    Update icolor.h

--- icolor.h
+++ icolor.h
@@ -2,22 +2,22 @@
 #define ICOLOR_H
 #include <QColor>
 
-class IColor
+class IColor// color product
 {
 public:
-    virtual QList <QColor>* GetColors(int colorCont) = 0;
+    virtual QList <QColor>* GetColors(int colorCont) = 0;//get data colorCont - count color
 };
 
-class Colored : public IColor
+class Colored : public IColor//colored color 
 {
 public:
-    QList <QColor>* GetColors(int colorCont);
+    QList <QColor>* GetColors(int colorCont);//get data colorCont - count color
 };
 
 class Monohrom :public IColor
 {
 public:
-    QList <QColor>* GetColors(int colorCont);
+    QList <QColor>* GetColors(int colorCont);//get data colorCont - count color
 };
 
 #endif // ICOLOR_H
